0ebabfea77b498a23fa139e0949f2fda57712664
hanzhi713 <hanzhi713@163.com>
2019-11-28 15:26:12 -0500

    most of io done

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
+// @ts-check
 /**
  * @type {HTMLCanvasElement}
  */
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvasUser = document.getElementById('canvas');
+canvasUser.width = 720;
+const ctxUser = canvasUser.getContext('2d');
 
-// canvas.getContext('2d').getImageData();
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvasAlgo = document.getElementById('algo');
+const ctxAlgo = canvasAlgo.getContext('2d');
+
+/**
+ * @type {HTMLInputElement}
+ */
+const targetSizeInput = document.getElementById('target-size');
+/**
+ * @type {HTMLInputElement}
+ */
+const thresholdInput = document.getElementById('threshold');
+
+const img = new Image();
+
+function draw() {
+    canvasAlgo.width = +targetSizeInput.value;
+    const ratio = img.height / img.width;
+    canvasUser.height = ratio * canvasUser.width;
+    canvasAlgo.height = ratio * canvasAlgo.width;
+    ctxUser.drawImage(img, 0, 0, canvasUser.width, canvasUser.height);
+    ctxAlgo.drawImage(img, 0, 0, canvasAlgo.width, canvasAlgo.height);
+}
+img.onload = draw
 
 function handleImage(input) {
     const reader = new FileReader();
     reader.onload = function (event) {
-        const img = new Image();
-        img.onload = function () {
-            canvas.width = img.width;
-            canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
-        }
         img.src = event.target.result;
     }
     reader.readAsDataURL(input.files[0]);
+}
+
+function thresh() {
+    draw();
+
+    const t = +thresholdInput.value;
+    const imgData = ctxAlgo.getImageData(0, 0, canvasAlgo.width, canvasAlgo.height);
+    const pixels = imgData.data;
+
+    for (let i = 0; i < pixels.length; i += 4) {
+        const avg = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
+        pixels[i] = pixels[i + 1] = pixels[i + 2] = avg > t ? 255 : 0;
+    }
+
+    ctxAlgo.putImageData(imgData, 0, 0);
+    ctxUser.imageSmoothingEnabled = false;
+    ctxUser.drawImage(canvasAlgo, 0, 0, canvasUser.width, canvasUser.height);
 }
