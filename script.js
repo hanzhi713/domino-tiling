4a99099d83bf1c84ff8a2fdba890183cd60d8acd
hanzhi713 <hanzhi713@163.com>
2019-12-08 21:44:23 -0500

    major functionality complete

--- script.js
+++ script.js
@@ -1,4 +1,57 @@
 // @ts-check
+
+const _allColors = [
+    '#f7867e',
+    '#82677E',
+    '#2C577C',
+    '#00a0a0',
+    '#355dff',
+    '#7790ff'
+];
+
+const allColors = _allColors.map(x => (parseInt(x.substring(1), 16) << 8) | 255);
+
+const defaultColors = _allColors.concat();
+
+window.onload = () => {
+    const colors = document.getElementById('colors');
+    for (let i = 0; i < 6; i++) {
+        const div = document.createElement('div');
+        colors.appendChild(div);
+
+        // Simple example, see optional options for more configuration.
+        const pickr = Pickr.create({
+            el: div,
+            theme: 'nano', // or 'monolith', or 'nano'
+            appClass: 'pkr-dis',
+            swatches: defaultColors,
+            default: defaultColors[i],
+            components: {
+
+                // Main components
+                preview: true,
+                opacity: true,
+                hue: true,
+
+                // Input / output Options
+                interaction: {
+                    hex: true,
+                    rgba: true,
+                    hsla: true,
+                    hsva: true,
+                    cmyk: true,
+                    input: true,
+                    save: true
+                }
+            }
+        });
+        pickr.on('save', (color, instance) => {
+            const temp = color.toRGBA();
+            allColors[i] = (temp[0] << 24) | (temp[1] << 16) | (temp[2] << 8) | (temp[3] * 255); 
+            instance.hide();
+        });
+    }
+}
 /**
  * @type {HTMLCanvasElement}
  */
@@ -15,7 +68,11 @@ const ctxAlgo = canvasAlgo.getContext('2d');
 /**
  * @type {HTMLInputElement}
  */
-const targetSizeInput = document.getElementById('target-size');
+const targetWidthInput = document.getElementById('target-width');
+/**
+ * @type {HTMLInputElement}
+ */
+const targetHeightInput = document.getElementById('target-height');
 /**
  * @type {HTMLInputElement}
  */
@@ -23,22 +80,58 @@ const thresholdInput = document.getElementById('threshold');
 /**
  * @type {HTMLInputElement}
  */
-const opacityInput = document.getElementById('opacity');
+const keepAspectRatio = document.getElementById('keep-ratio');
 
 const img = new Image();
 
+targetWidthInput.onchange = function () {
+    if (keepAspectRatio.checked) {
+        targetHeightInput.value = (img.height / img.width * +targetWidthInput.value).toString();
+    }
+}
+
+targetHeightInput.onchange = function () {
+    if (keepAspectRatio.checked) {
+        targetWidthInput.value = (img.width / img.height * +targetHeightInput.value).toString();
+    }
+}
+
+function fixWH() {
+    let w = +targetWidthInput.value, h = +targetHeightInput.value;
+    const ratio = img.width / img.height;
+    const temp = keepAspectRatio.checked;
+    keepAspectRatio.checked = false;
+    if (!w && !h) {
+        w = img.width;
+        h = img.height;
+    } else if (!h) {
+        h = w / ratio;
+    } else if (!w) {
+        w = h * ratio;
+    }
+    targetHeightInput.value = h.toFixed(0);
+    targetWidthInput.value = w.toFixed(0);
+    keepAspectRatio.checked = temp;
+    return [w, h];
+}
+
 function draw() {
-    canvasAlgo.width = +targetSizeInput.value;
-    const ratio = img.height / img.width;
+    const [w, h] = fixWH();
+    const ratio = h / w;
+    canvasAlgo.width = w;
+    canvasAlgo.height = h;
     canvasUser.height = ratio * canvasUser.width;
-    canvasAlgo.height = ratio * canvasAlgo.width;
 
     ctxAlgo.imageSmoothingEnabled = false;
-    ctxUser.drawImage(img, 0, 0, canvasUser.width, canvasUser.height);
-    ctxUser.imageSmoothingEnabled = false;
+    ctxAlgo.clearRect(0, 0, canvasUser.width, canvasUser.height);
     ctxAlgo.drawImage(img, 0, 0, canvasAlgo.width, canvasAlgo.height);
+
+    ctxUser.imageSmoothingEnabled = false;
+    ctxUser.clearRect(0, 0, canvasUser.width, canvasUser.height);
+    ctxUser.drawImage(img, 0, 0, canvasUser.width, canvasUser.height);
 }
-img.onload = draw
+
+img.onload = draw;
 
 function handleImage(input) {
     const reader = new FileReader();
@@ -66,32 +159,15 @@ function thresh() {
     ctxUser.drawImage(canvasAlgo, 0, 0, canvasUser.width, canvasUser.height);
 }
 
-const allColors = [
-    '#f7867e',
-    '#ffa000',
-    '#82677E',
-    '#2C577C',
-    '#6D838A',
-    '#00a0a0',
-    '#355dff',
-    '#7790ff',
-    '#9B5656',
-    '#CC9393',
-    '#993D5F'
-];
-
 /**
- * 
- * @param {Int32Array} matching 
+ * @param {number[][]} matching 
  */
 function dominoes(matching) {
     const width = canvasAlgo.width;
     const height = canvasAlgo.height;
 
     const colors = new Uint8Array(width * height);
-    for (let i = 0; i < matching.length; i += 2) {
-        const node1 = matching[i];
-        const node2 = matching[i + 1];
+    for (const [node1, node2] of matching) {
 
         const n1 = colors[node1 - 1];
         const n2 = colors[node1 + 1];
@@ -107,11 +183,9 @@ function dominoes(matching) {
         colors[node1] = colors[node2] = j;
     }
 
-    const opacity = +opacityInput.value;
     const temp = new DataView(new ArrayBuffer(allColors.length * 4));
     for (let i = 0; i < allColors.length; i++) {
-        const color = allColors[i];
-        temp.setUint32(i * 4, (parseInt(color.substring(1), 16) << 8) | opacity, true);
+        temp.setUint32(i * 4, allColors[i], true);
     }
 
     const imgData = ctxAlgo.getImageData(0, 0, canvasAlgo.width, canvasAlgo.height);
@@ -129,6 +203,54 @@ function dominoes(matching) {
     ctxUser.drawImage(canvasAlgo, 0, 0, canvasUser.width, canvasUser.height);
 }
 
-function test() {
-    dominoes([1, 2, 3, 4, 5, 6, 7, 8]);
+function drawTiling() {
+    thresh();
+
+    const imgData = ctxAlgo.getImageData(0, 0, canvasAlgo.width, canvasAlgo.height);
+    const pixels = imgData.data;
+
+    const edges = [];
+    const r = canvasAlgo.height, c = canvasAlgo.width;
+    let a = 0, b = 0;
+    const labelMap = new Int32Array(r * c).fill(-1);
+    const revMap1 = [];
+    const revMap2 = [];
+    for (let i = 0; i < r; i++) {
+        for (let j = 0; j < c; j++) {
+            const current = i * c + j;
+            if (pixels[current << 2] === 0 && pixels[(current << 2) + 3] !== 0) {
+                const temp = (i & 1) ^ (j & 1);
+                if (temp) {
+                    labelMap[current] = a++;
+                    revMap1.push(current);
+                } else {
+                    2
+                    labelMap[current] = b++;
+                    revMap2.push(current);
+                }
+            }
+        }
+    }
+    for (let i = 0; i < r; i++) {
+        for (let j = 0; j < c; j++) {
+            const current = i * c + j;
+            const currentLabel = labelMap[current];
+            if ((i & 1) ^ (j & 1) && currentLabel !== -1) {
+                if (j > 0 && labelMap[current - 1] !== -1) {
+                    edges.push([currentLabel, labelMap[current - 1]]);
+                }
+                if (j < c - 1 && labelMap[current + 1] !== -1) {
+                    edges.push([currentLabel, labelMap[current + 1]]);
+                }
+                if (i > 0 && labelMap[current - c] !== -1) {
+                    edges.push([currentLabel, labelMap[current - c]]);
+                }
+                if (i < r - 1 && labelMap[current + c] !== -1) {
+                    edges.push([currentLabel, labelMap[current + c]]);
+                }
+            }
+        }
+    }
+    const result = bipartiteMatching(a, b, edges);
+    dominoes(result.map(([x1, x2]) => [revMap1[x1], revMap2[x2]]));
 }
